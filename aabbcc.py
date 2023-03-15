b21f418ad27ad67b36ef43cc091cc91d30cd689b
FedotovS <svyatoslavfdtv@yandex.ru>
2023-03-15 20:03:40 +0500

    zsdgdrfhfgnрпасьп

--- aabbcc.py
+++ aabbcc.py
@@ -145,6 +145,31 @@ def news_delete(id):
     return redirect('/')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 def main():
     db_name = input()  # "db/Mars.db"
     db_session.global_init(db_name)
